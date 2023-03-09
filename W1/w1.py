4bcd893724ba3105d50de85e3de5423e9e27ba9c
Banzerino <ismailozen191@gmail.com>
2023-03-09 23:57:46 +0300

    Create w1.py

---
+++ W1/w1.py
@@ -0,0 +1,40 @@
+
+
+# ##  VERİ TİPLERİ
+
+
+# print = Yazdırma 
+
+# type = Veri tipini öğrenme
+
+# int = Tam sayı
+
+# float = Ondalıklı sayı
+
+# str = String(Metin)
+
+# bool = Boolean (Doğru veya yanlış)
+
+
+
+# progressBar = 0
+
+
+# dersProgrami = input("Ders programını tamamladınız mı True/False ?")
+# degerlendirme = input("Değerlendirmeyi tamamladınız mı True/False ?")
+# odev1 = input("Ödev 1'i tamamladınız mı True/False ?")
+# odev2 = input("Ödev 2'yi tamamladınız mı True/False ?")
+
+# if(dersProgrami== "True" or dersProgrami== "true" ):
+#     progressBar += 25
+
+# if (degerlendirme == "True" or degerlendirme == "true"):
+#     progressBar += 25
+
+# if (odev1 == "True" or odev1 == "true"):
+#     progressBar += 25
+
+# if (odev2 == "True"or odev2 == "true"):
+#     progressBar += 25
+
+# print(f"{progressBar}% tamamlandı")
